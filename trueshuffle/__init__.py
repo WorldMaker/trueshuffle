1aab9492f22dffc6777398a61cce2b8fd52235c6
Max Battcher <me@worldmaker.net>
2018-04-02 22:19:20 -0400

    :boot: Fix module bootstrap

--- trueshuffle/__init__.py
+++ trueshuffle/__init__.py
@@ -1,1 +1,15 @@
-from __main__ import main
+"""
+trueshuffle
+-----------
+
+Shuffles the files of a music library to a destination such as a thumbdrive. It attempts to be a *true* shuffle by
+storing a manifest list of all of the files that have previously been copied.
+"""
+
+if __name__ == 'main':
+    import os
+    import sys
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+    from trueshuffle.__main__ import main
+    main()
